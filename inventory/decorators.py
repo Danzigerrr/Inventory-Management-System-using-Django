934b9de72291eb54754ecaf7213669f2f6e12c70
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-07-30 11:41:33 +0200

    update decorators and profile view

--- inventory/decorators.py
+++ inventory/decorators.py
@@ -5,7 +5,7 @@ from functools import wraps
 def admin_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.username == 'Admin':
+        if request.user.is_authenticated and request.user.username == 'admin':
             return view_func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden("You are not allowed to access this page.")
