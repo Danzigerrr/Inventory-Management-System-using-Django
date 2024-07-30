00eaef751edec15495a25e3acde84c4c0a1af6cf
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-07-30 10:41:44 +0200

    create models: Product and InventoryTransaction

--- inventory/models.py
+++ inventory/models.py
@@ -2,17 +2,24 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+    stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
 
 
 class InventoryTransaction(models.Model):
+    TRANSACTION_TYPE_CHOICES = [
+        ('RECEIVE', 'Receive'),
+        ('SELL', 'Sell'),
+    ]
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    transaction_type = models.CharField(max_length=10, choices=[('IN', 'In'), ('OUT', 'Out')])
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
+    quantity = models.PositiveIntegerField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.transaction_type} {self.quantity} of {self.product.name}"
