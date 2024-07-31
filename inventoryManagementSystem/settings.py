6051720a6c9e1689c8ad087562b192f05e51d8d2
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-07-31 10:17:29 +0200

    update settings.py with staticfiles directory settings

--- inventoryManagementSystem/settings.py
+++ inventoryManagementSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.inventoryManagementSystem.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.inventoryManagementSystem.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.inventoryManagementSystem.com/en/5.0/ref/settings/#default-auto-field
